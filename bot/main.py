81eeab76006fd94b1356a63def19551ca451b649
Лидия Береза <lbereza@zunami.ru>
2024-05-20 23:29:13 +0200

    update

--- bot/main.py
+++ bot/main.py
@@ -9,9 +9,9 @@ dp = Dispatcher()
 
 @dp.message(Command('start'))
 async def start(message: Message):
-    keyboard = ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text='Создать претензию', web_app=WebAppInfo(url='https://ya.ru'))]],
+    keyboard = ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text='Создать претензию', web_app=WebAppInfo(url='https://lib-z.github.io/web_app/site/'))]],
                                resize_keyboard=True)
-    inlineKeyboard = InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text='Создать претензию', web_app=WebAppInfo(url='https://ya.ru'))]])
+    inlineKeyboard = InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text='Создать претензию', web_app=WebAppInfo(url='https://lib-z.github.io/web_app/site/'))]])
     await message.answer('Добрый день!', reply_markup=keyboard)
     await message.answer('Для создания претензии нажмите кнопку \"Создать претензию\"', reply_markup=inlineKeyboard)
 
